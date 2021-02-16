2201502d7a177281b0e2a34415b23e905d2b61bd
Jeremy Muriel <jeremy@jeremm.fr>
2021-02-16 16:26:46 +0100

    add wallix-bastion_device_localdomain_account resource

--- bastion/provider.go
+++ bastion/provider.go
@@ -45,6 +45,7 @@ func Provider() *schema.Provider {
 		ResourcesMap: map[string]*schema.Resource{
 			"wallix-bastion_device":                                resourceDevice(),
 			"wallix-bastion_device_localdomain":                    resourceDeviceLocalDomain(),
+			"wallix-bastion_device_localdomain_account":            resourceDeviceLocalDomainAccount(),
 			"wallix-bastion_externalauth_ldap":                     resourceExternalAuthLdap(),
 			"wallix-bastion_user":                                  resourceUser(),
 			"wallix-bastion_usergroup":                             resourceUserGroup(),
