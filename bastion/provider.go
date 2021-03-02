3dbd7cf664687c572303ac708448621d7a86a681
Jeremy Muriel <jeremy@jeremm.fr>
2021-03-02 16:36:28 +0100

    add ldapdomain resource

--- bastion/provider.go
+++ bastion/provider.go
@@ -53,6 +53,7 @@ func Provider() *schema.Provider {
 			"wallix-bastion_domain_account":                        resourceDomainAccount(),
 			"wallix-bastion_domain_account_credential":             resourceDomainAccountCredential(),
 			"wallix-bastion_externalauth_ldap":                     resourceExternalAuthLdap(),
+			"wallix-bastion_ldapdomain":                            resourceLdapDomain(),
 			"wallix-bastion_targetgroup":                           resourceTargetGroup(),
 			"wallix-bastion_user":                                  resourceUser(),
 			"wallix-bastion_usergroup":                             resourceUserGroup(),
