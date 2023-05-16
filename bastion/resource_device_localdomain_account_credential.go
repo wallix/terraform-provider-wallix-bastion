31f12ed5dd536bbaf70ee86e79b913e423eb4272
Jeremy Muriel <jeremy@jeremm.fr>
2023-05-16 10:03:31 +0200

    fix typo in error messages
    tpye -> type

--- bastion/resource_device_localdomain_account_credential.go
+++ bastion/resource_device_localdomain_account_credential.go
@@ -115,7 +115,7 @@ func resourceDeviceLocalDomainAccountCredentialCreate(
 		return diag.FromErr(err)
 	}
 	if ex {
-		return diag.FromErr(fmt.Errorf("credential tpye %s on account_id %s, domain_id %s, device_id %s already exists",
+		return diag.FromErr(fmt.Errorf("credential type %s on account_id %s, domain_id %s, device_id %s already exists",
 			d.Get("type").(string), d.Get("account_id").(string), d.Get("domain_id").(string), d.Get("device_id").(string)))
 	}
 	err = addDeviceLocalDomainAccountCredential(ctx, d, m)
@@ -129,7 +129,7 @@ func resourceDeviceLocalDomainAccountCredentialCreate(
 	}
 	if !ex {
 		return diag.FromErr(fmt.Errorf(
-			"credential tpye %s on account_id %s, domain_id %s, device_id %s not found after POST",
+			"credential type %s on account_id %s, domain_id %s, device_id %s not found after POST",
 			d.Get("type").(string), d.Get("account_id").(string), d.Get("domain_id").(string), d.Get("device_id").(string)))
 	}
 	d.SetId(id)
