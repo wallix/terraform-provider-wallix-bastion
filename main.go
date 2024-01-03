d36f18c4740c84598f24f6fee12ac5c544ff3fb7
bsimon <bsimon@wallix.com>
2024-01-03 11:10:10 +0100

    Fixed old reference to claranet repository

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/claranet/terraform-provider-wallix-bastion/bastion"
+	"github.com/wallix/terraform-provider-wallix-bastion/bastion"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
